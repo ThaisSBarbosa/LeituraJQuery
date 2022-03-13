2d3ec0663508a10abf92f14ad8c9d4509c396947
ThaisSBarbosa <thssantosbarbosa@gmail.com>
2022-03-13 20:02:15 -0300

    Versão Inicial

---
+++ src/br/edu/cefsa/n1b1/Principal.java
@@ -0,0 +1,24 @@
+package br.edu.cefsa.n1b1;
+
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) throws Exception {
+		String allString = leArquivoJQuery();				
+		
+		List<String> listaDeTokens = SeparadorDeTokens.DivideTokens(allString);
+		listaDeTokens.forEach(System.out::println);
+	}
+
+	private static String leArquivoJQuery() throws Exception {
+		Scanner scanner = new Scanner(new File("jquery.txt"));
+		String allString = "";
+		while (scanner.hasNextLine()) {
+			allString += scanner.nextLine();
+		}
+		return allString;
+	}
+}
---
+++ src/br/edu/cefsa/n1b1/SeparadorDeTokens.java
@@ -0,0 +1,37 @@
+package br.edu.cefsa.n1b1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class SeparadorDeTokens {
+
+	public static List<String> DivideTokens(String textoCompleto) throws Exception{
+		textoCompleto = textoCompleto.replace("\r", "");
+		textoCompleto = textoCompleto.replace("\n", "");		
+		textoCompleto = textoCompleto.replace("\t", "");
+		textoCompleto = textoCompleto.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
+
+		
+		textoCompleto = textoCompleto.replace(",", " , ");
+		textoCompleto = textoCompleto.replace(";", " ; ");
+		textoCompleto = textoCompleto.replace("{", " { ");
+		textoCompleto = textoCompleto.replace("}", " } ");
+		textoCompleto = textoCompleto.replace("(", " ( ");
+		textoCompleto = textoCompleto.replace(")", " ) ");
+		textoCompleto = textoCompleto.replace(":", " : ");
+		textoCompleto = textoCompleto.replace("?", " ? ");
+
+		var listComVazios = Arrays.asList(textoCompleto.split("[" + Pattern.quote(" #@_\\/*") + "]+"));
+
+		List<String> listSemVazios = new ArrayList<>();
+		for (String item : listComVazios) {
+			if (item != "")
+				listSemVazios.add(item);
+		}
+		
+		return listSemVazios;
+	}
+	
+}
