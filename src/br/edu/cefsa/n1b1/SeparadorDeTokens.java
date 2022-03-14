12d951ca259c81c394310b6137e373319ea94ec1
fabidota <62340981+fabidota@users.noreply.github.com>
2022-03-14 17:03:17 -0300

    Alterado lógica das strings + arquivo info

--- src/br/edu/cefsa/n1b1/SeparadorDeTokens.java
+++ src/br/edu/cefsa/n1b1/SeparadorDeTokens.java
@@ -14,6 +14,9 @@ public class SeparadorDeTokens {
 		textoCompleto = textoCompleto.replace("\t", "");
 		textoCompleto = textoCompleto.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
 
+		textoCompleto = textoCompleto.replace("+", " + ");
+		textoCompleto = textoCompleto.replace("-", " - ");
+		//textoCompleto = textoCompleto.replace("\"", " \" ");
 		
 		textoCompleto = textoCompleto.replace(",", " , ");
 		textoCompleto = textoCompleto.replace(";", " ; ");
@@ -37,7 +40,42 @@ public class SeparadorDeTokens {
 				listSemVazios.add(item);
 		}
 		
-		return listSemVazios;
+		// teste pega strings com aspas
+		List<String> listStringsCompletas = new ArrayList<>();
+		//listStringsCompletas = listSemVazios;
+		int index = 0;
+		while (index<listSemVazios.size())
+		{
+			String texto = listSemVazios.get(index);
+			
+			if ((texto.startsWith("\"") && texto.endsWith("\"") && texto.length()>1 )  || !texto.startsWith("\"")) {
+
+				listStringsCompletas.add(texto);
+				index++;
+			} else if (texto.startsWith("\""))
+			{
+				int procuraProximo=index+1;
+				boolean achou=false;
+				while (! achou) {
+					if (listSemVazios.get(procuraProximo).endsWith("\"")) {
+						texto= texto.replace(texto, texto+" "+listSemVazios.get(procuraProximo));
+						listStringsCompletas.add(texto);
+						achou=true;
+						index=procuraProximo+1;
+					} else {
+						texto=texto.replace(texto, texto+" "+listSemVazios.get(procuraProximo));
+						procuraProximo++;
+					}
+				}
+			} 
+			
+		}
+		// return listSemVazios;
+		return listStringsCompletas;
+	}
+	
+	private String mudaString(String antiga, String nova){
+	    return antiga+" "+nova;
 	}
 	
 }
